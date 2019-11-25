c51e575741de3206a0596788cb2d7db380f885a2
Michał Klepacki <mike@MacBook-Pro-Micha.local>
2019-11-25 19:37:26 +0100

    Check if interpreter works

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 import random
+import requests
 
 def random_int():
     print(random.randint(1, 100))
 
+def get_response(url):
+    print(requests.get(url))
+
 if __name__ == "__main__":
     random_int()
+    get_response('https://docs.pylint.org/en/1.6.0/tutorial.html')
