de6679350ea595e4fb30512dbeb29a3f1b98d2c7
Michał Klepacki <mike@MacBook-Pro-Micha.local>
2019-11-30 11:08:27 +0100

    November - 30

--- hello.py
+++ hello.py
@@ -8,10 +8,6 @@ def random_int():
     """ Print random number from 1 to 100 """
     print(random.randint(1, 100))
 
-def get_response(url):
-    """ Print response from the provided url """
-    print(requests.get(url))
-
 if __name__ == "__main__":
+    print(__name__)
     random_int()
-    get_response('https://docs.pylint.org/en/1.6.0/tutorial.html')
